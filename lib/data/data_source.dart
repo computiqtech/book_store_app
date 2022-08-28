ae4381b2737850144ff042d7f7c975b423217417
devsadeq <dev.sadeq@gmail.com>
2022-08-28 21:05:53 +0300

    prepare store data

---
+++ lib/data/data_source.dart
@@ -0,0 +1,50 @@
+import 'package:get/get.dart';
+
+import 'models/book.dart';
+
+class DataSource {
+  static RxList<Book> localBooks = <Book>[
+    Book(
+      title: "A Tale of Two Cities",
+      author: "Charles Dickens",
+      cover: "https://api.lorem.space/image/book?w=150&h=220&hash=8B7BCDC2",
+      rate: 5.0,
+      price: 99.99,
+    ),
+    Book(
+      title: "The Little Prince",
+      author: "Antoine de Saint-Exupéry",
+      cover: "https://api.lorem.space/image/book?w=150&h=220&hash=8B7BCDC2",
+      rate: 4.3,
+      price: 46.99,
+    ),
+    Book(
+      title: "Harry Potter and the Philosopher's Stone",
+      author: "J.K.Rowling",
+      cover: "https://api.lorem.space/image/book?w=150&h=220&hash=8B7BCDC2",
+      rate: 3.0,
+      price: 64.99,
+    ),
+    Book(
+      title: "Mockingjay",
+      author: "Suzanne Collins",
+      cover: "https://api.lorem.space/image/book?w=150&h=220&hash=8B7BCDC2",
+      rate: 4.0,
+      price: 52.99,
+    ),
+    Book(
+      title: "Me Before You",
+      author: "Jojo Moyes",
+      cover: "https://api.lorem.space/image/book?w=150&h=220&hash=8B7BCDC2",
+      rate: 4.9,
+      price: 30.99,
+    ),
+    Book(
+      title: "Harry Potter and the Philosopher's Stone",
+      author: "J.K.Rowling",
+      cover: "https://api.lorem.space/image/book?w=150&h=220&hash=8B7BCDC2",
+      rate: 3.0,
+      price: 64.99,
+    ),
+  ].obs;
+}
