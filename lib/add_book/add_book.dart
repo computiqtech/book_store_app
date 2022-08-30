35b26f57835e245ff7e93258d7066e7b00b6bfd3
Sura Atta <96949252+SuraAtta@users.noreply.github.com>
2022-08-30 16:39:32 +0300

    Update add_book.dart

--- lib/add_book/add_book.dart
+++ lib/add_book/add_book.dart
@@ -49,7 +49,7 @@ class _AddBookState extends State<AddBook> {
       body:Column(
         children: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(32, 125, 0, 0),
+            padding: const EdgeInsets.fromLTRB(32, 125, 0, 20),
             child: Align(
                 alignment: Alignment.centerLeft,
                 child:
@@ -59,6 +59,7 @@ class _AddBookState extends State<AddBook> {
           Expanded(
                 child:ListView(
                       controller: scrollController,
+                      padding: EdgeInsets.zero,
                       children: [
                         Container(
                           height: 600,
